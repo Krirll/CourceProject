1503f3deecf886492052420b08e05ec95a618cb7
Krirll <balalaev20022@gmail.com>
2021-05-12 22:13:57 +0300

    add an icon application

--- CourseProject/obj/Debug/CourseProject_Content.g.i.cs
+++ CourseProject/obj/Debug/CourseProject_Content.g.i.cs
@@ -8,7 +8,6 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("modeldb.context.tt")]
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("modeldb.tt")]
+[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("sun218.ico")]
 
 
